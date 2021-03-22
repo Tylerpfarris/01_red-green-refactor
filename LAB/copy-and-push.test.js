bd3410cd9d0b826ea4ea045896cc1fe95ca186d8
Tyler Farris <Tylerpfarris@gmail.com>
2021-03-22 14:14:31 -0700

    array is not mutated

--- LAB/copy-and-push.test.js
+++ LAB/copy-and-push.test.js
@@ -1,11 +1,16 @@
-const numbers = [1, 2, 3];
-
 const copyAndPush = require('./copy-and-push');
+const numbers = [1, 2, 3];
+const orgArr = numbers;
 
 describe('copy and push function', () => {
-  it('returns a new array with all the items in  the origonal array and a new item pushed to the end', () => {
+  it('returns a new array with all the items in  the original array and a new item pushed to the end', () => {
     const newArr = copyAndPush(numbers, 4);
-
-    expect(newArr).toEqual(...numbers, 4);
+   
+    expect(newArr).toEqual(...orgArr, 4);
   });
+    
+  it('should not mutate the original array', () => {
+
+    expect(orgArr).toEqual(numbers);
+  }) ;
 });
