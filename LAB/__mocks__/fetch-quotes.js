498f857546d525d1bc2b80f06b1150498c62d6c5
Tyler Farris <Tylerpfarris@gmail.com>
2021-03-22 17:52:21 -0700

    stretch done

---
+++ LAB/__mocks__/fetch-quotes.js
@@ -0,0 +1,15 @@
+
+
+const fetchQuotes = () => {
+  
+
+  return Promise.resolve({
+    character: expect.any(String),
+    quote: expect.any(String),
+    image: expect.any(String)
+          
+  });
+};
+
+module.exports = fetchQuotes;
+
--- LAB/fetch-quotes.test.js
+++ LAB/fetch-quotes.test.js
@@ -1,16 +1,32 @@
-const fetchQuotes = require('./fetch-quotes');
+
+jest.mock('node-fetch');
+const fetch = require('node-fetch');
+const fetchQuotes = require('./fetch-quotes.js');
 
 
+fetch.mockImplementation(() => {
+  return Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          character: 'Professor Farnsworth',
+          quote: 'Everyone, I have a dramatic announcement. Anyone with a weak heart should\nleave. Goodbye.',
+          image: 'https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/farnsworth.png'
+        }
+      ])
+  });
+});
 describe('A function that fetches quotes', () => {
   it('Uses the Futurama Quote API to return a single quote in the right format', async() => {
-      
+    
     const quotes = await fetchQuotes();
       
+    
     expect(quotes).toEqual(expect.objectContaining({
       character: expect.any(String),
       quote: expect.any(String),
       image: expect.any(String)
-          
     }));
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 });
